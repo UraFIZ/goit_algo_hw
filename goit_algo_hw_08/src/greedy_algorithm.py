4ab377d266a24cd7a9a556bf97104a1f93903057
urafiz <kratser88@gmail.com>
2024-08-16 15:15:06 +0200

    replace with the correct tasks

--- goit_algo_hw_08/src/greedy_algorithm.py
+++
@@ -1,8 +0,0 @@
-def find_coins_greedy(amount, coins):
-    result = {}
-    for coin in sorted(coins, reverse=True):
-        if amount >= coin:
-            count = amount // coin
-            result[coin] = count
-            amount -= coin * count
-    return result
